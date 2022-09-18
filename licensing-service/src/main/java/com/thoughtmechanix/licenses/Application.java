be92837d9dfba61d4de6e224494f712eeed627f8
genhaojiu <yunxiyao_zh@163.com>
2022-09-18 11:05:25 +0800

    删除licensing-service工程里无效引用utils

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
@@ -2,7 +2,6 @@ package com.thoughtmechanix.licenses;
 
 import com.thoughtmechanix.licenses.config.ServiceConfig;
 import com.thoughtmechanix.licenses.events.models.OrganizationChangeModel;
-import com.thoughtmechanix.licenses.utils.UserContextInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
